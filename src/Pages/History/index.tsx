2554b26d57e144cdf4b9a7de0ca873b3ebb85217
Maike <maikegomesalmeida@outlook.com>
2023-01-24 23:06:57 -0300

    deletando arquivos

--- src/Pages/History/index.tsx
+++
@@ -1,9 +0,0 @@
-import { Testando } from './styles'
-
-export function History() {
-  return (
-    <div>
-      <Testando>History</Testando>
-    </div>
-  )
-}
--- src/Pages/History/styles.tsx
+++
@@ -1,5 +0,0 @@
-import styled from 'styled-components'
-
-export const Testando = styled.h1`
-  font-size: 5rem;
-`
