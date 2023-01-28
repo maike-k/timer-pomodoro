cc1415c2a5abf5d0953c4f89f2900e171b331c4d
Maike <maikegomesalmeida@outlook.com>
2023-01-28 19:07:59 -0300

    Page Home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -15,14 +15,27 @@ export function Home() {
       <form action="">
         <FormContainer>
           <label htmlFor="task">Vou trabalhar em</label>
-          <TaskInput id="task" placeholder="Dê um nome para o seu projeto" />
-
+          <TaskInput
+            id="task"
+            list="task_suggestion"
+            placeholder="Dê um nome para o seu projeto"
+          />
+          <datalist id="task_suggestion">
+            <option value="Projeto 1" />
+            <option value="Projeto 2" />
+            <option value="Banana" />
+            <option value="Maça" />
+          </datalist>
           <label htmlFor="minutesAmount">durante</label>
-          <MinutesAllowed type="number" id="minutesAmount" />
-
+          <MinutesAllowed
+            type="number"
+            id="minutesAmount"
+            step={5}
+            min={5}
+            max={60}
+          />
           <span>minutos.</span>
         </FormContainer>
-
         <CountDownContainer>
           <span>0</span>
           <span>0</span>
