2203eef7d1383b0987e7c31f7538e8a25a6cb51d
Maike <maikegomesalmeida@outlook.com>
2023-01-28 09:51:24 -0300

    layout-rotas

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,13 @@
+import { HeaderContainer } from './styles'
+
+export function Header() {
+  return (
+    <HeaderContainer>
+      <span>Menu</span>
+      <nav>
+        <a href="">timer</a>
+        <a href="">history</a>
+      </nav>
+    </HeaderContainer>
+  )
+}
--- src/components/Router.tsx
+++ src/components/Router.tsx
@@ -1,12 +1,12 @@
 import { Routes, Route } from 'react-router-dom'
-import { DefaulLayout } from '../layouts/DefaultLayout'
+import { DefaultLayout } from './DefaultLayout'
 import { History } from '../pages/History'
 import { Home } from '../pages/Home'
 
 export function Router() {
   return (
     <Routes>
-      <Route path="/" element={<DefaulLayout />}>
+      <Route path="/" element={<DefaultLayout />}>
         <Route path="/" element={<Home />} />
         <Route path="/history" element={<History />} />
       </Route>
