1a648c52b54ee4dd6b580065f7c1b8e8841bdb2c
Maike <maikegomesalmeida@outlook.com>
2023-06-01 10:06:49 -0300

    Finalizando validações com react-hook-form

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,6 +1,9 @@
 // Cada página é um component, dentro da minha apliacação.
-
 import { Play } from 'phosphor-react'
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { z } from 'zod'
+
 import {
   CountDownContainer,
   FormContainer,
@@ -11,10 +14,37 @@ import {
   TaskInput
 } from './styles'
 
+const newCycleFormValidationSchema = z.object({
+  task: z.string().min(1, 'Informe a tarefa'),
+  minutesAmount: z
+    .number()
+    .min(5, 'O ciclo precisa ser de no mínimo 5 minutos')
+    .max(60, 'O ciclo precisa ser de no máximo 60 minutos')
+})
+
+// Typescript Schema Data
+type NewCycleFormData = z.infer<typeof newCycleFormValidationSchema>
+
 export const Home = () => {
+  const { register, handleSubmit, watch, reset } = useForm<NewCycleFormData>({
+    resolver: zodResolver(newCycleFormValidationSchema),
+    defaultValues: {
+      task: '',
+      minutesAmount: 0
+    }
+  })
+
+  const handleCreateNewCycle = (data: NewCycleFormData) => {
+    console.log(data)
+    reset()
+  }
+
+  const task = watch('task')
+  const isSubmitDisabled = !task
+
   return (
     <HomeContainer>
-      <form>
+      <form onSubmit={handleSubmit(handleCreateNewCycle)}>
         <FormContainer>
           <label htmlFor="task">Vou trabalhar em</label>
           <TaskInput
@@ -22,6 +52,7 @@ export const Home = () => {
             id="task"
             placeholder="Dê um nome para o seu projeto"
             list="task-suggestion"
+            {...register('task')}
           />
 
           <datalist id="task-suggestion">
@@ -39,6 +70,7 @@ export const Home = () => {
             step={5}
             min={5}
             max={60}
+            {...register('minutesAmount', { valueAsNumber: true })}
           />
           <span>minutos.</span>
         </FormContainer>
@@ -51,7 +83,7 @@ export const Home = () => {
           <span>0</span>
         </CountDownContainer>
 
-        <Start type="submit" disabled>
+        <Start type="submit" disabled={isSubmitDisabled}>
           <Play size={24} /> Start
         </Start>
       </form>
